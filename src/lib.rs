9993697b9deff386d99754edade8911bac507642
eV <ev@7pr.xyz>
2019-03-26 05:31:13 +0000

    update to use wip traits

--- src/lib.rs
+++ src/lib.rs
@@ -2,12 +2,12 @@
 
 extern crate embedded_hal as hal;
 
-use hal::spi::{FullDuplex, Mode, Phase, Polarity};
+use hal::{
+    blocking::spi::Write,
+    spi::{Mode, Phase, Polarity},
+};
 
-use smart_leds_trait::{Color, SmartLedsWrite};
-
-use nb;
-use nb::block;
+use smart_leds_trait::{SmartLedsWrite, RGB8};
 
 /// SPI mode that can be used for this crate
 ///
@@ -24,47 +24,41 @@ pub struct P9813<SPI> {
 
 impl<SPI, E> P9813<SPI>
 where
-    SPI: FullDuplex<u8, Error = E>,
+    SPI: Write<u8, Error = E>,
 {
     pub fn new(spi: SPI) -> P9813<SPI> {
         Self { spi }
     }
 
     fn flush(&mut self) -> Result<(), E> {
-        for _ in 0..20 {
-            block!(self.spi.send(0))?;
-            block!(self.spi.read())?;
-        }
+        self.spi.write(&[0; 20])?;
         Ok(())
     }
 }
 
 impl<SPI, E> SmartLedsWrite for P9813<SPI>
 where
-    SPI: FullDuplex<u8, Error = E>,
+    SPI: Write<u8, Error = E>,
 {
+    type Pixel = RGB8;
     type Error = E;
+
     /// Write all the items of an iterator to a p9813 strip
-    fn write<T>(&mut self, iterator: T) -> Result<(), E>
+    fn write<T, I>(&mut self, iterator: T) -> Result<(), E>
     where
-        T: Iterator<Item = Color>,
+        T: Iterator<Item = I>,
+        I: Into<RGB8>,
     {
         self.flush()?;
 
         for item in iterator {
+            let item = item.into();
             let r = item.r;
             let g = item.g;
             let b = item.b;
             let top = 0xC0 | ((!b & 0xC0) >> 2) | ((!g & 0xC0) >> 4) | ((!r & 0xC0) >> 6);
 
-            block!(self.spi.send(top))?;
-            block!(self.spi.read())?;
-            block!(self.spi.send(b))?;
-            block!(self.spi.read())?;
-            block!(self.spi.send(g))?;
-            block!(self.spi.read())?;
-            block!(self.spi.send(r))?;
-            block!(self.spi.read())?;
+            self.spi.write(&[top, b, g, r])?;
         }
 
         self.flush()?;
